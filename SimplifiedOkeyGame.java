3f529e3ae8ccca409471d7e6b5bf6021e122fe8e
Ferit Bilgi <126424534+feritbilgi75@users.noreply.github.com>
2024-02-25 15:34:31 +0300

    Simplified okey game 5 methods have done

---
+++ SimplifiedOkeyGame.java
@@ -0,0 +1,191 @@
+public class SimplifiedOkeyGame {
+
+    Player[] players;
+    Tile[] tiles;
+    int tileCount;
+
+    Tile lastDiscardedTile;
+
+    int currentPlayerIndex = 0;
+
+    public SimplifiedOkeyGame() {
+        players = new Player[4];
+    }
+
+    public void createTiles() {
+        tiles = new Tile[104];
+        int currentTile = 0;
+
+        // four copies of each value, no jokers
+        for (int i = 1; i <= 26; i++) {
+            for (int j = 0; j < 4; j++) {
+                tiles[currentTile++] = new Tile(i);
+            }
+        }
+
+        tileCount = 104;
+    }
+
+    /*
+     * TODO: distributes the starting tiles to the players
+     * player at index 0 gets 15 tiles and starts first
+     * other players get 14 tiles, this method assumes the tiles are already shuffled
+     */
+    public void distributeTilesToPlayers() {
+
+    }
+
+    /*
+     * TODO: get the last discarded tile for the current player
+     * (this simulates picking up the tile discarded by the previous player)
+     * it should return the toString method of the tile so that we can print what we picked
+     */
+    public String getLastDiscardedTile() {
+        return null;
+    }
+
+    /*
+     * TODO: get the top tile from tiles array for the current player
+     * that tile is no longer in the tiles array (this simulates picking up the top tile)
+     * and it will be given to the current player
+     * returns the toString method of the tile so that we can print what we picked
+     */
+    public String getTopTile() {
+        return null;
+    }
+
+    /*
+     * TODO: should randomly shuffle the tiles array before game starts
+     */
+    public void shuffleTiles() {
+
+    }
+
+
+    //Buradan itibaren yap
+
+    /*
+     * TODO: check if game still continues, should return true if current player
+     * finished the game. use checkWinning method of the player class to determine
+     */
+    public boolean didGameFinish() {
+        boolean didFinish = false;
+        if (players[getCurrentPlayerIndex()].checkWinning()){
+            didFinish = true;
+        }
+        return didFinish;
+    }
+
+    /* TODO: finds the player who has the highest number for the longest chain
+     * if multiple players have the same length may return multiple players
+     */
+    public Player[] getPlayerWithHighestLongestChain() {
+        
+        Player[] winners2 = new Player[4];
+        int index = 0;
+        winners2[index] = players[0];
+        for (int i = 1; i < 4 ; i++){
+            if (winners2[index].findLongestChain() < players[i].findLongestChain()){
+                winners2[index] = players[i];
+                winners2[1] = null;
+                winners2[2] = null;
+                winners2[3] = null;
+            }
+            else if (winners2[index].findLongestChain() == players[i].findLongestChain()){
+                index++;
+                winners2[index] = players[i];
+            }
+        }
+
+        Player[] winners = new Player[index];
+        for (int i = 0; i < index; i++){
+            winners[i] = winners2[i];
+        }
+        return winners;
+    }
+    
+    /*
+     * checks if there are more tiles on the stack to continue the game
+     */
+    public boolean hasMoreTileInStack() {
+        return tileCount != 0;
+    }
+
+    /*
+     * TODO: pick a tile for the current computer player using one of the following:
+     * - picking from the tiles array using getTopTile()
+     * - picking from the lastDiscardedTile using getLastDiscardedTile()
+     * you should check if getting the discarded tile is useful for the computer
+     * by checking if it increases the longest chain length, if not get the top tile
+     */
+    public void pickTileForComputer() {
+        boolean taken = false;
+
+        for (int i = 0; i < 14; i++){
+            if (lastDiscardedTile.canFormChainWith(tiles[i])){
+                getLastDiscardedTile();
+                taken = true;
+                break;
+            }
+        }
+        if (taken == false){
+            getTopTile();
+        }
+    }
+
+    /*
+     * TODO: Current computer player will discard the least useful tile.
+     * you may choose based on how useful each tile is
+     */
+    public void discardTileForComputer() {
+        for (int i = 0; i < 14; i++){
+            int sync = 0;
+            for (int j = i+1; j < 14; j++){
+                if (tiles[j].canFormChainWith(tiles[j])){
+                    sync++;
+                }
+            }
+            if (sync == 0){
+                discardTile(i);
+            }
+        }
+    }
+
+    /*
+     * TODO: discards the current player's tile at given index
+     * this should set lastDiscardedTile variable and remove that tile from
+     * that player's tiles
+     */
+    public void discardTile(int tileIndex) {
+        tiles[tileIndex] = null;
+    }
+
+    public void displayDiscardInformation() {
+        if(lastDiscardedTile != null) {
+            System.out.println("Last Discarded: " + lastDiscardedTile.toString());
+        }
+    }
+
+    public void displayCurrentPlayersTiles() {
+        players[currentPlayerIndex].displayTiles();
+    }
+
+    public int getCurrentPlayerIndex() {
+        return currentPlayerIndex;
+    }
+
+      public String getCurrentPlayerName() {
+        return players[currentPlayerIndex].getName();
+    }
+
+    public void passTurnToNextPlayer() {
+        currentPlayerIndex = (currentPlayerIndex + 1) % 4;
+    }
+
+    public void setPlayerName(int index, String name) {
+        if(index >= 0 && index <= 3) {
+            players[index] = new Player(name);
+        }
+    }
+
+}
